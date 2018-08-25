00c0bb6a0a90c256ace58a758bd53dd746625ef1
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-08-25 11:52:17 +0200

    Update info2.py

--- info2.py
+++ info2.py
@@ -24,6 +24,11 @@ class Members():
         em.set_thumbnail(url=member.avatar_url)
         await self.client.say(embed=em)
         
+    @commands.command(pass_context=True)
+    async def changelog(self):
+      em = discord.Embed()
+    em.add_field(name='0.8. and previous versions', value='I don"t remeber everything what I"ve done in every version')
+    em.add_field(name='0.9.',  value='added a new Cog called Info2')
 
 def setup(client):
     client.add_cog(Members(client))
