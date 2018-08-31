eb3e41225ef2e767f3df6e2a8741124e20320ce0
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-08-31 20:45:36 +0200

    Update thanks.py

--- thanks.py
+++ thanks.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
 
+class Members():
+    def __init__(self, client):
+        self.client = client
 
-@commands.command(name="thanks",
-                aliases = ["Thanks"],
-                pass_context= True)
-async def tanks(self, ctx):
-    tanks = discord.Embed(name = "Special thanks to:" description = "Adytzu96#3030 , \n everyone who is using this Bot!")
-    await client.say(embed = tanks)
+    @commands.command(name="thanks",
+                    aliases = ["Thanks"],
+                    pass_context= True)
+    async def tanks(self, ctx):
+        tanks = discord.Embed(name = "Special thanks to:" description = "Adytzu96#3030 , \n everyone who is using this Bot!")
+        await client.say(embed = tanks)
