e4ecce05fb5c5fb41b74b9a7fde7b8bae50fa88a
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-08-31 20:53:31 +0200

    Update thanks.py

--- thanks.py
+++ thanks.py
@@ -10,7 +10,7 @@ class Thanks():
                     pass_context= True)
     async def tanks(self, ctx):
         tanks = discord.Embed(name = "Special thanks to:", description = "Adytzu96#3030 , \n everyone who is using this Bot!")
-        await client.say(embed = tanks)
+        await self.client.say(embed = tanks)
 
     
 def setup(client):
