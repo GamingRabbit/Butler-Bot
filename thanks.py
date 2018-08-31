f5994e40078ee6acd63aa164224d54d0f705431b
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-08-31 20:44:21 +0200

    Update thanks.py

--- thanks.py
+++ thanks.py
@@ -1,1 +1,6 @@
-
+@commands.command(name="thanks",
+                aliases = ["Thanks"],
+                pass_context= True)
+async def tanks(self, ctx):
+    tanks = discord.Embed(name = "Special thanks to:" description = "Adytzu96#3030 , \n everyone who is using this Bot!")
+    await client.say(embed = tanks)
