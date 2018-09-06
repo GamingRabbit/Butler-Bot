7c5376e7ea6546f7c12f11d64f3f403625fbda81
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-09-06 16:24:18 +0200

    Update bbotic.py

--- bbotic.py
+++ bbotic.py
@@ -18,6 +18,7 @@ c2 = discord.Client()
 client.load_extension('info2')
 client.load_extension('thanks')
 client.load_extension('dabotlist')
+client.load_extension('eh')
 @client.event
 async def on_ready():
     await client.change_presence(game=Game(name="Use James,commands"))
