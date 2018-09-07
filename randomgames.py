dfc262cbd0944e34d695ccb2d82acebbfb72942c
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-09-07 15:22:57 +0200

    Update randomgames.py

--- randomgames.py
+++ randomgames.py
@@ -6,9 +6,9 @@ class rg():
         self.client = client
         
     @commands.command()
-    async def gg(self, ctx):
+    async def gg(self, message):
         x = random.randint(1,3)
-        if ctx.message.content == 'James,gg ' + x:
+        if message.content == 'James,gg ' + x:
             await self.client.say("You've Won  and guessed the number, here is your reward: a nice :cookie:")
         else:
             await self.client.say("Nope, wrong number")
