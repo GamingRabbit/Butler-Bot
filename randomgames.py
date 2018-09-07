5ab4ea1f78b5cdb52345fb181f02ccb27fa1e17d
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-09-07 15:26:51 +0200

    Update randomgames.py

--- randomgames.py
+++ randomgames.py
@@ -4,13 +4,13 @@ import random
 class rg():
     def __init__(self, client):
         self.client = client
-   @commands.command()     
-   async def gg(message):
-    rand = random.randint(0,2)
-    user_choice=message
-    choices = ["1", "2", "3"]
-    outcome_list = ["won", "lose!", "lose."]
-    result =(choices.index(user_choice)+rand)%3
-    await client.say("I chose {}, you {}".format(choices[result], outcome_list[rand]))
+    @commands.command()     
+    async def gg(self, message):
+     rand = random.randint(0,2)
+     user_choice=message
+     choices = ["1", "2", "3"]
+     outcome_list = ["won", "lose!", "lose."]
+     result =(choices.index(user_choice)+rand)%3
+     await self.client.say("I chose {}, you {}".format(choices[result], outcome_list[rand]))
 def setup(client):
     client.add_cog(rg(client))
