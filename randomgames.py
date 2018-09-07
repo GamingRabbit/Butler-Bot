1d652a29423cd535b4b5e6f1db0bab499505de10
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-09-07 15:29:15 +0200

    Update randomgames.py

--- randomgames.py
+++ randomgames.py
@@ -9,7 +9,7 @@ class rg():
      rand = random.randint(0,2)
      user_choice=message
      choices = ["1", "2", "3"]
-     outcome_list = ["won", "lose!", "lose."]
+     outcome_list = ["win, heres your reward, a :cookie:", "lose!", "lose."]
      result =(choices.index(user_choice)+rand)%3
      await self.client.say("I chose {}, you {}".format(choices[result], outcome_list[rand]))
 def setup(client):
