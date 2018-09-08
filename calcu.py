390dc90a190d8ff79110d0e8f5f3a0041b288a19
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-09-08 11:08:59 +0200

    Update calcu.py

--- calcu.py
+++ calcu.py
@@ -5,17 +5,17 @@ class Calculator():
     def __init__(self, client):
         self.client = client
         
-     @commands.command(self)
-     async def add(leftn : int, rightn : int):
-         await self.client.say(leftn + rightn)
+    @commands.command(self)
+    async def add(leftn : int, rightn : int):
+        await self.client.say(leftn + rightn)
         
-     @commands.command(self)
-     async def multiply(leftn : int, rightn : int):
-         await self.client.say(leftn * rightn)
+    @commands.command(self)
+    async def multiply(leftn : int, rightn : int):
+        await self.client.say(leftn * rightn)
         
-     @commands.command(self)
-     async def sub(leftn : int, rightn : int):
-         await self.client.say(leftn - rightn)
+    @commands.command(self)
+    async def sub(leftn : int, rightn : int):
+        await self.client.say(leftn - rightn)
         
 def setup(client):
     client.add_cog(Calculator(client))
