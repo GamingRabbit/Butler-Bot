2e194ab2fd9a6b1e65bdba96d22621041a4a64c5
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-09-08 11:01:28 +0200

    Update calcu.py

--- calcu.py
+++ calcu.py
@@ -4,3 +4,7 @@ from discord.ext import commands
 class Calculator():
     def __init__(self, client):
         self.client = client
+        
+    @commands.command(self)
+    async def add(leftn : int, rightn : int):
+        await self.client.say(leftn + rightn)
