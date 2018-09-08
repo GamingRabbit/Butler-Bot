377b9a9c9af6dac0da319b8d752e65757003bbfc
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-09-08 11:10:22 +0200

    Update calcu.py

--- calcu.py
+++ calcu.py
@@ -5,16 +5,16 @@ class Calculator():
     def __init__(self, client):
         self.client = client
         
-    @commands.command(self)
-    async def add(leftn : int, rightn : int):
+    @commands.command()
+    async def add(self, leftn : int, rightn : int):
         await self.client.say(leftn + rightn)
         
-    @commands.command(self)
-    async def multiply(leftn : int, rightn : int):
+    @commands.command()
+    async def multiply(self, leftn : int, rightn : int):
         await self.client.say(leftn * rightn)
         
-    @commands.command(self)
-    async def sub(leftn : int, rightn : int):
+    @commands.command()
+    async def sub(self, leftn : int, rightn : int):
         await self.client.say(leftn - rightn)
         
 def setup(client):
