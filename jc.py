d33bb76d9dd7306850407994cd549de56e313db1
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-09-09 15:58:35 +0200

    Update jc.py

--- jc.py
+++ jc.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import cter
 
-class Don_t_Use_The_default_help_command():
+class Don_t_Use_The_Default_Help_Command():
     def __init__(self, client):
         self.client = client
     
@@ -11,3 +11,6 @@ class Don_t_Use_The_default_help_command():
         await self.client.say("Use James,commands !")
         cter.counter +=1
         await self.client.say("btw, you are the" + counter "user who used this command")
+        
+def setup(client):
+    client.add_cog(Don_t_Use_The_Default_Help_Command(client))
