a4b4fac748cd4be58fcf1c72c24a02b71741694f
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-09-09 15:55:28 +0200

    Update jc.py

--- jc.py
+++ jc.py
@@ -4,3 +4,8 @@ from discord.ext import commands
 class Don_t_Use_The_default_help_command():
     def __init__(self, client):
         self.client = client
+    
+    @commands.command()
+    async def Use_James_commands():
+        await client.say("Use James,commands !")
+        
