c705b09145f49e49cec23623b9c38c67486f32fa
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-09-11 11:09:33 +0200

    Update bbotic.py

--- bbotic.py
+++ bbotic.py
@@ -22,7 +22,6 @@ client.load_extension('dabotlist')
 client.load_extension('randomgames')
 client.load_extension('calcu')
 client.load_extension('jc')
-client.load_extension('morsetwo')
 @client.event
 async def on_ready():
     await client.change_presence(game=Game(name="Use James,commands"))
