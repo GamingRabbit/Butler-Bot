dea5bb4fe770bc7458c2edaa8f70b743cd66fe55
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-09-11 10:12:50 +0200

    Update morse.py

--- morse.py
+++ morse.py
@@ -1,21 +1,25 @@
 import discord
 from discord.ext import commands
-
-class Morse():
-    def __init__(self, client):
-        self.client = client
-        
-    @commands.command(aliases = ["morsetotext", "MTT", "Mtt", "mTt", "mtT", "mTT"], pass_context = True)
-    async def mtt(self, ctx, message):
-      morsetext = message
-      code = {}
-      for l in morsetext:
-        w = l.split()
-        code[w[0]] = w[1]
-        
-      for i in range(97,123):
-        code[chr(i)] = code[chr(i - 32)]
-      await self.client.say(code)
+import sys
     
-def setup(client):
-    client.add_cog(Morse(client))
+def mtt():
+  try:
+    d = open("m.txt")
+  except:
+    Print("error")
+  l = d.readlines()
+  d.close
+        
+  code = {}
+  for l in morsetext:
+    w = l.split()
+    code[w[0]] = w[1]
+        
+  for i in range(97,123):
+    code[chr(i)] = code[chr(i - 32)]
+      
+  return code
+    
+    
+    
+
