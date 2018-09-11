b6c35e3204338cf2ed37f65b4195507e9b901ab1
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-09-11 10:40:40 +0200

    Update morsetwo.py

--- morsetwo.py
+++ morsetwo.py
@@ -6,7 +6,7 @@ class Morse():
     def __init__(self, client):
         self.client = client
         
-    @property 
+     
     async def wcode(self, txt, code):
         code = morse.mtt()
         for z in txt:
@@ -18,7 +18,7 @@ class Morse():
     @commands.command()
     async def rcode(self, *, message):
         code = morse.mtt()
-        wcode(message, code)
+        await wcode(message, code)
         
 def setup(client):
     client.add_cog(Morse(client))
