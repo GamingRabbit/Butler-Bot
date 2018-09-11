405b1b0f991ada196c1cfb2e2b3cb221a886799d
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-09-11 12:54:00 +0200

    Update morsetwo.py

--- morsetwo.py
+++ morsetwo.py
@@ -1,4 +1,4 @@
-import discord
+'''import discord
 from discord.ext import commands
 import sys
 import morse
@@ -19,5 +19,5 @@ class Morse():
         
 def setup(client):
     client.add_cog(Morse(client))
-        
+   '''     
 
