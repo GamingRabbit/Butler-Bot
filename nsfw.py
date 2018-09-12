05130fac116e6a82e7de4fea0d8ea3f41f1c3547
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-09-12 18:01:52 +0200

    Update nsfw.py

--- nsfw.py
+++ nsfw.py
@@ -6,12 +6,14 @@ class nsfw():
         self.client = client
  
     @commands.command(pass_context = True)
-    async def rule34(self, ctx ,*,message : discord.message):
+    async def rule34(self, ctx ,*,message):
         if 'nsfw' in ctx.message.channel.name:
             if message == None:
-                await self.client.say('https://rule34.xxx/index.php?page=post&s=random')
+                embed = discord.Embed(title = 'results', url = 'https://rule34.xxx/index.php?page=post&s=random')  
+                await self.client.say(embed = embed)
             else:
-                await self.client.say('https://rule34.xxx/index.php?page=post&s=list&tags={0}'.format(message))
+                embed = discord.Embed(title = 'results', url = 'https://rule34.xxx/index.php?page=post&s=list&tags={0}'.format(message))
+                await self.client.say(embed = embed)
                 
         else:
             await self.client.say("This channel doesn't has nsfw inside it's name. Please note that I can't check IF a nsfw channel is a nsfw channel. I can only check if 'nsfw' is in the channel name.") 
