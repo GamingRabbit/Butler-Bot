d1ae80be83cbe38e0617a525352d34c2f046a7fa
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-10-03 13:21:21 +0200

    Update support.py

--- support.py
+++ support.py
@@ -1,5 +1,13 @@
+import discord
+from discord.ext import commands
+
+class Calculator():
+    def __init__(self, client):
+        self.client = client
 
 
 
 
-join my (new) Server : https://discord.gg/Te6g5XF
+        @commands.command()
+        async def support(self):
+          await client.say("join my support Server : https://discord.gg/Te6g5XF")
