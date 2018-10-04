3f1b13f03f0c668818e2938e206ea39b7dfd82a1
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-10-04 17:06:07 +0200

    Update cb.py

--- cb.py
+++ cb.py
@@ -1,6 +1,18 @@
 import discord
 from discord.ext import commands
+import random
 
 class rds():
     def __init__(self, client):
         self.client = client
+
+    @commands.commands()
+    async def rs(self):
+        wordI=[
+            "I",
+            "You",
+            "It",
+            "He",
+            "She"
+        ]
+        await self.client.say(random.choice(wordI))
