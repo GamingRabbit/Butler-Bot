da5e6618e80fe46bb444029d8804aed5faff4a48
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-10-29 20:37:53 +0100

    Update cb.py

--- cb.py
+++ cb.py
@@ -18,7 +18,24 @@ class rds():
         wordII=[
             "will",
             "wont"]
-        await self.client.say(random.choice(wordI)+" "+random.choice(wordII))
+        wordIII=[
+            "get",
+            "destroy",
+            "kill",
+            "buy you a",
+            "paint your face green, print a toothbrush, record a video with it and make a toast for"
+        ]
+        wordIIII=[
+            "Donald Trump",
+            "You",
+            "Me",
+            "it",
+            "a video card",
+            "your Video card",
+            "my energy drink",
+            "**__~~*E*~~__**"
+        ]
+        await self.client.say(random.choice(wordI)+" "+random.choice(wordII)+" "+random.choice(wordIII)+" "+random.choice(wordIIII))
         
 def setup(client):
     client.add_cog(rds(client))
